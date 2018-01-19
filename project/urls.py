1c7ffadef07424009a76336015d4c7d318a28157
david mark <markkariuki17@gmail.com>
2018-01-19 11:59:47 +0300

    add change

--- project/urls.py
+++ project/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    url('^$',views.Welcome,name = 'Welcome'),
+    url('^today/$',views.pictures_of_day,name='picturesToday')
 ]
--- project/views.py
+++ project/views.py
@@ -1,6 +1,42 @@
 from django.shortcuts import render
-form django.http import HttpResponse
+from django.http import HttpResponse
+import datetime as dt
 
 # Create your views here.
 def Welcome(request):
-    reutrn HttpResponse('welcome to my portfoio project')
+    return HttpResponse('welcome to my unsplash project')
+
+def pictures_today(request):
+    date = dt.date.today()
+    return render(request, 'all-news/today-pictures.html', {"date": date,})
+
+# View Function to present news from past days
+def past_days_pictures(request, past_date):
+
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(news_today)
+
+    return render(request, 'all-news/past-pictures.html', {"date": date})
+
+def news_of_day(request):
+    date = dt.date.today()
+
+    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
