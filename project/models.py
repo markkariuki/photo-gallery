5f86c3e881fe2219df6dfa18b413c7983d5f2d6f
david mark <markkariuki17@gmail.com>
2018-01-19 15:18:11 +0300

    add the photos model

--- project/models.py
+++ project/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
-
-class tags(models.Model):
-    first_name = models.CharField(max_length =30)
-    last_name = models.CharField(max_length =30)
-    email = models.EmailField()
+class photos(models.Model):
+    image = models.ImageField(upload_to = 'photos/', blank = True)
+    name = models.CharField(max_length=30)
+    descripton = models.TextField()
+    location_taken = models.ForeignKey(Location)
+    category = models.ManyToManyField(categories)
