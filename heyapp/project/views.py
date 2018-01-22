330e19d87e42ad528e39c44f494e208a5a274d33
david mark <markkariuki17@gmail.com>
2018-01-22 08:02:29 +0300

    add change to the views.py file

--- heyapp/project/views.py
+++ heyapp/project/views.py
@@ -6,29 +6,10 @@ import datetime as dt
 # Create your views here.
 
 
-def allphotos(request):
-    photos = Photos.objects.all()
-    return render(request, 'today-pictures.html', {'photos':photos})
 
 # View Function to present news from past days
-def past_days_pictures(request,past_date):
-
-    try:
-        # Converts data from the string Url
-        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
-
-    except ValueError:
-        # Raise 404 error when ValueError is thrown
-        raise Http404()
-        assert False
-
-    if date == dt.date.today():
-        return redirect(pictures_today)
-
-    project = photos.days_pictures(date)
-    return render(request, 'all-pics/today-pictures.html', {"date": date})
 
 def pictures_today(request):
     date = dt.date.today()
-    project = photos.todays_pictures()
-    return render(request, 'all-pics/today-pictures.html', {"date": date,"project":project})
+    photos = photos.objects.all()
+    return render(request, 'all-pics/today-pictures.html', {"photos": photos})
