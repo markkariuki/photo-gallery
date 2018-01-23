ca67bcf165ab9891ac75df52bd63ac049fe0c729
david mark <markkariuki17@gmail.com>
2018-01-23 15:51:40 +0300

    add the url for search

--- heyapp/project/urls.py
+++ heyapp/project/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns=[
     url(r'^$', views.pictures_today, name='picturesToday'),
     url(r'^photo/(\d+)', views.single_photo, name='singlePhoto')
+    url(r'^search/', views.search_results, name='search_results')
+
 ]
