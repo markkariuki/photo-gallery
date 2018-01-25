4ac3a7942343a1bf9a53862d624b4915f8fb2a5a
david mark <markkariuki17@gmail.com>
2018-01-25 15:58:26 +0300

    add changes

--- heyapp/heyapp/settings.py
+++ heyapp/heyapp/settings.py
@@ -22,11 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ibna$va*$m5!oe2r_so)%&1y4je22am3(n(-64i1d%7z19pjxq'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = []
 
 
