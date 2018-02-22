c8a03d6a9f1296003f6eae2d0cdd07ae7bdcf3de
david mark <markkariuki17@gmail.com>
2018-02-22 15:16:08 +0300

    sdd change to the deployment

--- heyapp/settings.py
+++ heyapp/settings.py
@@ -26,7 +26,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = []
 
 
@@ -134,7 +134,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-Static files (CSS, JavaScript, Images)
 STATICFILES_DIRS = (
 os.path.join(PROJECT_ROOT, 'static'),)
 
